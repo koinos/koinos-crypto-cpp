be8f050255aaf43848de4d8b18d2dadf13417733
Steve Gerbino <steve@gerbino.co>
2021-08-09 16:07:32 -0400

    Bump Hunter version and specify CapnProto version

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -42,3 +42,9 @@ hunter_config(libsecp256k1
    SHA1 "0534fa8948f279b26fd102905215a56f0ad7fa18"
 )
 
+hunter_config(CapnProto
+   VERSION "0.8.0"
+   URL "https://capnproto.org/capnproto-c++-0.8.0.tar.gz"
+   SHA1 fbc1c65b32748029f1a09783d3ebe9d496d5fcc4
+)
+
